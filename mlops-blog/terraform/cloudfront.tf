49efafc48c4cf8db326d5626ca9d31a96b55804d
cobriensr <charles.a.obrien@outlook.com>
2025-07-27 17:09:58 -0500

    fixed cloudfront page access issues

--- mlops-blog/terraform/cloudfront.tf
+++ mlops-blog/terraform/cloudfront.tf
@@ -1,3 +1,31 @@
+// cloudfront.tf
+
+# CloudFront Function for URL rewriting
+resource "aws_cloudfront_function" "url_rewrite" {
+  name    = "buildmlops-url-rewrite"
+  runtime = "cloudfront-js-2.0"
+  comment = "URL rewrite for Next.js static export"
+  publish = true
+
+  code = <<EOF
+function handler(event) {
+    var request = event.request;
+    var uri = request.uri;
+    
+    // Check if the URI ends with a slash
+    if (uri.endsWith('/')) {
+        request.uri += 'index.html';
+    }
+    // If no file extension and not ending with slash, add /index.html
+    else if (!uri.includes('.')) {
+        request.uri += '/index.html';
+    }
+    
+    return request;
+}
+EOF
+}
+
 # CloudFront Origin Access Control
 resource "aws_cloudfront_origin_access_control" "blog" {
   name                              = "${var.domain_name}-oac"
@@ -24,6 +52,12 @@ resource "aws_cloudfront_distribution" "blog" {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
+
+    # Add the CloudFront function
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.url_rewrite.arn
+    }
 
     forwarded_values {
       query_string = false
@@ -60,11 +94,18 @@ resource "aws_cloudfront_distribution" "blog" {
     compress               = true
   }
 
-  # Custom error pages for SPA routing
+  # Custom error page for 404s
   custom_error_response {
     error_code         = 404
     response_code      = 404
-    response_page_path = "/404.html"
+    response_page_path = "/404/index.html"
+  }
+
+  # Custom error page for 403s (treat as 404)
+  custom_error_response {
+    error_code         = 403
+    response_code      = 404
+    response_page_path = "/404/index.html"
   }
 
   restrictions {
@@ -82,7 +123,7 @@ resource "aws_cloudfront_distribution" "blog" {
   depends_on = [aws_acm_certificate_validation.cert]
 }
 
-# CloudFront for redirect
+# CloudFront for redirect (unchanged)
 resource "aws_cloudfront_distribution" "redirect" {
   enabled         = true
   is_ipv6_enabled = true
--- mlops-blog/terraform/locals.tf
+++ mlops-blog/terraform/locals.tf
@@ -1,4 +1,4 @@
-// locals.terraform {
+// locals.tf
 
 locals {
   s3_origin_id = "S3-${var.domain_name}"
